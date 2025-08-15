5447d3e734675aaaf289047c01a5662878011cfc
jheyson <jheysonln_1997@hotmail.com>
2025-08-15 17:42:17 -0500

    ftr delete folders

--- src/app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { LoginForm } from '@/presentation/components/forms/login-form';
-
-/**
- * Página de login
- */
-export default function LoginPage() {
-  return <LoginForm />;
-}
--- src/app/(auth)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { RegisterForm } from '@/presentation/components/forms/register-form';
-
-/**
- * Página de registro
- */
-export default function RegisterPage() {
-  return <RegisterForm />;
-}
