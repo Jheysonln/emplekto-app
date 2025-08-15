1873253c4f4756499d2cffa0dc56a6a2afe23340
jheyson <jheysonln_1997@hotmail.com>
2025-08-15 17:39:08 -0500

    ftr-commit 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Providers } from '../app/providers';
-import { ErrorBoundary } from '@/presentation/components/common/error-boundary';
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -27,11 +24,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={inter.className}>
-        <ErrorBoundary>
-          <Providers>
             {children}
-          </Providers>
-        </ErrorBoundary>
       </body>
     </html>
   );
